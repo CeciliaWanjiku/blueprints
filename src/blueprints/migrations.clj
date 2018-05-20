579cfbcd7e6c0629f350187115bf9f73a7ab0fb6
Josh Lehman <jalehman37@gmail.com>
2018-05-20 09:20:10 -0700

    remove migrations

--- src/blueprints/migrations.clj
+++
@@ -1,9 +0,0 @@
-(ns blueprints.migrations
-  (:require [datomic.api :as d]
-            [io.rkn.conformity :as c]))
-
-(defn norms [conn part]
-  {})
-
-(defn conform [conn part]
-  (c/ensure-conforms conn (norms conn part)))
